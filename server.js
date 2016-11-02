544a004edaec04793bd32a4c058b7f33f578d8e9
Jagath Samarabandu <jagath@uwo.ca>
2016-11-02 18:47:47 +0000

    removed promise stuff

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.Promise = require('promise');
 mongoose.connect('mongodb://localhost:27017/bears');
 
 var Schema       = mongoose.Schema;
@@ -16,30 +15,18 @@ db.once('open', function() {
   console.log('Connected to database')// we're connected!
 });
 
-var saves=0;
-
 var b1 = new Bear();
-b1.name = "Baloo 3";
-b1.save().then(function(b){
-  console.log("Saved " + b.name);
-  saves++;
-});
+b1.name = "Baloo 1";
+b1.save();
 
 var b2 = new Bear();
-b2.name = "Beorn 3";
-b2.save().then(function(b){
-  console.log("Saved " + b.name);
-  saves++;
-});
+b2.name = "Beorn 1";
+b2.save();
 
 
 var b3 = new Bear();
-b3.name = "Corduroy 3";
-b3.save().then(function(b){
-  console.log("Saved " + b.name);
-  saves++;
-});
+b3.name = "Corduroy 1";
+b3.save();
 
-while (saves < 2) {}
 Bear.find( { name: /./}, console.log);
 //process.exit(0);
