cc16c13365891780a0b43a188669b77d4d288c8c
Jagath Samarabandu <jagath@uwo.ca>
2017-02-17 16:56:56 +0000

    More informative comments and logs

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+// Sample JavaScript code to use 'mongoose' library
+// Written by Jagath Samarabandu © 2016
 // Make sure that the database is running before starting the server
-// Use ./mongod command on a separate window and leave it running
+// Use ./start-mongodb command on a separate window and leave it running
 
-// Using mongoose middleware to interact with the database
+// Step 1: Using mongoose middleware to interact with the database
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/bears');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Connected to database')// we're connected!
+  console.log('1. Connected to database')// we're connected!
 });
 
 // Use 'bluebird' Promise library with mongoose
@@ -23,17 +25,20 @@ var BearSchema   = new Schema({
 // Create the prototype object that we will use to create all bears
 var Bear = mongoose.model('Bear', BearSchema);
 
-// Create the bear and set name and timestamp
+// Step 2a: Create a bear object and set its properties
 var b1 = new Bear();
 b1.name = "Winnie the Pooh";
 b1.time = (new Date()).toLocaleString();;
 
+// Step 2b: save the model
 // save() method returns a JavaScript promise. 
 // We use the .then() method of the promise to do something when promise is fulfilled
 b1.save().then(function(b) {
-  console.log("Saved " + b.name);
+  console.log("2. Saved " + b.name);
   Bear.find( { name: /./}, console.log);
 });
 
+// Step 3: Find all bears
 // This still gets run before the save method above has finished.
+console.log("3. Retrieving all records");
 Bear.find( { name: /./}, console.log);
